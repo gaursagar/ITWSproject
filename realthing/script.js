304c9c3b4dfd8c28a4aa1e3f310f28c7bb92d77f
sagar.gaur <sagar.gaur@cr13062.(none)>
2014-03-25 21:01:16 +0530

    +Timer +csv handling; -score calculator

--- realthing/script.js
+++ realthing/script.js
@@ -1,5 +1,40 @@
-document.addEventListener("load", readTextFile("file:///ug/ug2k13/cnd/sagar.gaur/scratchpad/realthing/file.txt"))
+document.addEventListener("load", readTextFile("file:///ug/ug2k13/cnd/sagar.gaur/scratchpad/realthing/file.csv"))
 
+function resetPage(dataArray)
+{
+    document.getElementById("counter").innerHTML = "0"
+    clearInterval(window.timerID)
+    clearInterval(window.flashfunction)
+    document.getElementById("x1").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x2").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x3").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x4").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x5").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x6").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x7").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x8").style.background = "rgba(102, 153, 147, 0.5)"
+    document.getElementById("x9").style.background = "rgba(102, 153, 147, 0.5)"
+    
+    document.getElementById("timer").innerHTML ="Start"
+    document.getElementById("x1").innerHTML =""
+    document.getElementById("x2").innerHTML =""
+    document.getElementById("x3").innerHTML =""
+    document.getElementById("x4").innerHTML =""
+    document.getElementById("x5").innerHTML =""
+    document.getElementById("x6").innerHTML =""
+    document.getElementById("x7").innerHTML =""
+    document.getElementById("x8").innerHTML =""
+    document.getElementById("x9").innerHTML =""
+    
+    $(document.body).click(function(evt)
+    {
+        if(evt.target.id == "timer")
+        {
+            //writeText(dataArray[Math.random() * dataArray.length])
+            writeText(dataArray)
+        }
+    })
+}
 
 function shuffle(o)
 {
@@ -9,35 +44,64 @@ function shuffle(o)
 
 function writeText(allText)
 {
-    shuffledText = shuffle(allText.split(" "))
+    //console.log(allText)
+    $(document.body).off('click')
+    correctText = allText[Math.floor(Math.random() * dataArray.length + 0)]
+    shuffledText = correctText.slice(0)
+    shuffledText = shuffle(shuffledText)
     console.log(shuffledText)
-    document.getElementById("b1").innerHTML =shuffledText[0]
-    document.getElementById("b2").innerHTML =shuffledText[1]
-    document.getElementById("b3").innerHTML =shuffledText[2]
-    document.getElementById("b4").innerHTML =shuffledText[3]
-    document.getElementById("b5").innerHTML =shuffledText[4]
-    document.getElementById("b6").innerHTML =shuffledText[5]
-    document.getElementById("b7").innerHTML =shuffledText[6]
-    document.getElementById("b8").innerHTML =shuffledText[7]
-    document.getElementById("b9").innerHTML =shuffledText[8]
+    document.getElementById("x1").innerHTML =shuffledText[0]
+    document.getElementById("x2").innerHTML =shuffledText[1]
+    document.getElementById("x3").innerHTML =shuffledText[2]
+    document.getElementById("x4").innerHTML =shuffledText[3]
+    document.getElementById("x5").innerHTML =shuffledText[4]
+    document.getElementById("x6").innerHTML =shuffledText[5]
+    document.getElementById("x7").innerHTML =shuffledText[6]
+    document.getElementById("x8").innerHTML =shuffledText[7]
+    document.getElementById("x9").innerHTML =shuffledText[8]
 
     timesClicked = 0
+    if(timesClicked == 0)
+    {
+        var start = new Date;
+        window.timerID = setInterval(function()
+        {   
+            $('#timer').text(((new Date - start) / 1000).toFixed(2));
+        }, 50);
+    }
+
     var clickOrder = new Array()
 
     $(document.body).click(function(evt){
         var clicked = evt.target
-        //console.log(evt.target.id)
+        console.log(evt.target.id)
         var currentID = clicked.id
-        timesClicked++
-        clickOrder.push(document.getElementById(currentID).innerHTML)              
-        if(timesClicked == 9)
+        if(currentID[0] == "x")
         {
-            if (clickOrder.join(" ") == allText)
-                alert("launde!!")
-            else
-                alert("dont make the same mistake your father has made")
-            timesClicked = 0
-        }
+            timesClicked++
+
+            clickOrder.push(document.getElementById(currentID).innerHTML)
+            document.getElementById(currentID).style.background = "rgba(102, 153, 147, 1)"
+            
+            window.flashfunction = setTimeout(function()
+            {            
+                document.getElementById(currentID).style.background = "rgba(212, 63, 87, 0.5)"
+            },100)
+
+            document.getElementById("counter").innerHTML =timesClicked
+            
+            if(timesClicked == 9)
+            {
+                console.log(clickOrder.join(" "))
+                console.log(correctText.join(" "))
+                if (clickOrder.join(" ") == correctText.join(" "))
+                    alert("launde!!")
+                else
+                    alert("dont make the same mistake your father has made")
+                $(document.body).off('click');
+                resetPage(allText)
+           }
+    }
         //alert(document.getElementById(currentID).innerHTML)
     })
 }
@@ -52,11 +116,19 @@ function readTextFile(file)
         {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
-                allText = rawFile.responseText;
+                data = rawFile.responseText;
                 console.log("Successfully Read")
-                writeText(allText)
+                readBeta(data)
             }
         }
     }
     rawFile.send(null);
+}
+
+function readBeta(allText)
+{
+    //console.log("here")
+    dataArray = $.csv.toArrays(data)
+    resetPage(dataArray)
+    //console.log(window.data)
 }
